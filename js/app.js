6c1127923f7dc8bdd21be702586b203283616044
Jhonah <buryakone@gmail.com>
2020-10-06 00:45:09 +0700

    рефакторинг под свой лад

--- js/app.js
+++ js/app.js
@@ -1,35 +1,22 @@
-// immediate invoked function expression
+const buttons = document.querySelectorAll('.btn');
+const imgs = document.querySelector('.img-container');
+const maxCount = 4;
+let count = 0;
 
-(function() {
-  const pictures = [
-    "contBcg-0",
-    "contBcg-1",
-    "contBcg-2",
-    "contBcg-3",
-    "contBcg-4"
-  ];
-
-  //select both left and right button. Add event listeners
-  const buttons = document.querySelectorAll('.btn')
-  const imageDIV = document.querySelector('.img-container')
-  let counter = 0
-
-  buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-      if (button.classList.contains('btn-left')){
-        counter--
-        if(counter < 0){
-          counter = pictures.length -1
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if(button.classList.contains('btn-left')) {
+            count--;
+            if (count < 0) {
+                count = maxCount;
+            }
+        } else if (button.classList.contains('btn-right')) {
+            count++;
+            if (count > maxCount) {
+                count = 0;
+            }
         }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
-      if (button.classList.contains('btn-right')){
-        counter++
-        if(counter > pictures.length -1){
-          counter = 0
-        }
-        imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`
-      }
+        console.log(count);
+        imgs.style.backgroundImage = `url("./img/contBcg-${count}.jpeg")`;
     })
-  })
-})();
+})
